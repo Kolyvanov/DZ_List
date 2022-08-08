f563c5a3d4c27bfcbd60db67ec41a8d99c3f5715
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-08-08 12:27:38 +0300

    add search operation

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Scanner scanner1 = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Выберите операцию: \n1.Добавить покупку\n2.Показать покупки\n3.Удалить покупку");
+            System.out.println("Выберите операцию: \n1.Добавить покупку\n2.Показать покупки\n3.Удалить покупку\n4.Найти покупку");
 
             int number = scanner.nextInt();
             switch (number) {
@@ -21,7 +21,7 @@ public class Main {
                     break;
                 case 2:
                     System.out.println("Список покупок:");
-                    boolean empty = false;
+                    boolean empty = false; //флаг пустоты корзины покупок
                     for (int i = 0; i < list.size(); i++) {
                         if (list.get(i) != null) {
                             System.out.println((i + 1) + ". " + list.get(i));
@@ -38,12 +38,12 @@ public class Main {
                     for (int i = 0; i < list.size(); i++) {
                         System.out.println((i + 1) + ". " + list.get(i));
                     }
-                    int shopNumber;
+                    int shopNumber; //заводим номер покупки, выбранной для удаления
                     System.out.println("Какую покупку хотите удалить? Введите номер или название");
                     String shop = scanner1.nextLine();
                     try {
                         shopNumber = Integer.parseInt(shop);
-                        String shopRemove = list.get(shopNumber - 1);
+                        String shopRemove = list.get(shopNumber - 1); //сохраняем название покупки перед её удалением из списка
                         list.remove(shopNumber - 1);
 
                         System.out.println("Покупка " + '"' + shopRemove + '"' + " Удалена. Список покупок:");
@@ -56,6 +56,23 @@ public class Main {
                         for (int i = 0; i < list.size(); i++) {
                             System.out.println((i + 1) + ". " + list.get(i));
                         }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Введите текст для поиска:");
+                    input = scanner1.nextLine();
+                    String queryLower = input.toLowerCase();
+                    boolean empty1 = false; //флаг пустого результата поиска
+                    System.out.println("Найдено:");
+                    for (int i = 0; i < list.size(); i++) {
+                        String itemLower = list.get(i).toLowerCase();
+                        if (itemLower.contains(queryLower)) {
+                            empty1 = true;
+                            System.out.println((i + 1) + ". " + list.get(i));
+                        }
+                    }
+                    if (!empty1) {
+                        System.out.println("По вашему запросу покупок нет");
                     }
                     break;
             }
